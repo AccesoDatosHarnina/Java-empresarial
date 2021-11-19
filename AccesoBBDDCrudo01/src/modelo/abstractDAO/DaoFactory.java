dc0ee5ab940efcfdcc15a70b7aac795ef6a4788f
programacion <>
2021-11-19 10:15:21 +0100

    viernes

---
+++ AccesoBBDDCrudo01/src/modelo/abstractDAO/DaoFactory.java
@@ -0,0 +1,22 @@
+package modelo.abstractDAO;
+
+
+public abstract class DaoFactory {
+	public DaoFactory factory;
+
+	public DaoFactory() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public DaoFactory getFactory() {
+		return factory;
+	}
+
+	public void setFactory(DaoFactory factory) {
+		this.factory = factory;
+	}
+	
+	public abstract PersonaDAO getPersonaDAO();
+	
+}
