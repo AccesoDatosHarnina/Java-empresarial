268f0773f8f01b77d92620a3e3ebeac2f51a14e1
programacion <>
2021-12-02 13:59:57 +0100

    cambios para ejercicio

--- JPAH2herencia10/src/main/java/com/example/demo/modelo/PersonaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.modelo;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonaRepository extends CrudRepository<Persona, Long> {
-
-}
